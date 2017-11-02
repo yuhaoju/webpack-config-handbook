250681da5eacca43eee67767a0ca9407dd44509b
yuhao.ju <yuhao.ju@qunar.com>
2017-11-02 11:42:21 +0800

    feat: add externals example

---
+++ 12-externals/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    entry: './app.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    externals: {
+        jquery: 'window.$'
+    },
+    devServer: {
+        publicPath: "/dist/",
+        port: 3000
+    }
+}
