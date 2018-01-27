139129758888a9eabb660799aa73f0745a366d2f
yuhao.ju <yuhao.ju@qunar.com>
2018-01-27 15:35:25 +0800

    feat: add seperate css example

---
+++ 04-handle-styles/seperate-css/app.js
@@ -0,0 +1,3 @@
+import './style.css';
+
+document.write('My Webpack app.<br/>');
---
+++ 04-handle-styles/seperate-css/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './app.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("style.css")
+    ],
+    devServer: {
+        publicPath: '/dist/',
+        port: 3000
+    }
+}
