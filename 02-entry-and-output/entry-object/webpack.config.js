469e2d48205dfaab161126cac8c17b8a632af3e2
Yuhao Ju <yuhao@tubi.tv>
2018-03-28 22:42:42 +0800

    fix: make demos work again

--- 02-entry-and-output/entry-object/webpack.config.js
+++ 02-entry-and-output/entry-object/webpack.config.js
@@ -2,13 +2,13 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        app: './app.js', // 对象的 key 是入口名，value 是入口路径
-        just_entry_name: './components/list', // 入口名和实际路径并没有关系
-        'multiple/dir/entry': './components/detail' // 入口名可以是路径，这样相应的输出也在多级路径中
+        app: './app.js',
+        list: './components/list',
+        detail: './components/detail'
     },
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: '[name].js' // 根据入口名来决定输出资源的名字
+        filename: '[name].js'
     },
     devServer: {
         publicPath: "/dist/",
--- 04-handle-styles/scss/webpack.config.js
+++ 04-handle-styles/scss/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: './app.js',
@@ -28,9 +27,6 @@ module.exports = {
             }
         ]
     },
-    plugins: [
-        new ExtractTextPlugin("bundle.css")
-    ],
     devServer: {
         publicPath: '/dist/',
         port: 3000
