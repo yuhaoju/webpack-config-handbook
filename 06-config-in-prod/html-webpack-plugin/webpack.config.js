dc971817af666140b335d5942c6fbab8a4fc45dc
Yuhao Ju <yuhaoju0623@gmail.com>
2018-05-20 14:42:37 +0800

    update

---
+++ 06-config-in-prod/html-webpack-plugin/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './app.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle@[chunkhash].js'
+    },
+    mode: 'production',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './template.html'
+        })
+    ],
+    devServer: {
+        publicPath: '/dist/',
+        port: 3000
+    }
+};
