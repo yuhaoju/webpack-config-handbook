487b761b29889d3332b91293fa2b081c8c3e5c22
Yuhao Ju <yuhaoju0623@gmail.com>
2018-05-27 16:54:45 +0800

    fix: chapter2

--- 02-entry-and-output/entry-function/webpack.config.js
+++ 02-entry-and-output/entry-function/webpack.config.js
@@ -5,15 +5,15 @@ module.exports = {
   entry: () => ({
     partA: './partA.js',
     partB: './partB.js',
-    index: ['babel-polyfill', './index.js']
+    index: ['babel-polyfill', './index.js'],
   }),
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].js'
+    filename: '[name].js',
   },
-  plugins: [new htmlPlugin({ title: path.basename(__dirname) })],
+  plugins: [new htmlPlugin({ title: path.basename(__dirname), })],
   devServer: {
     publicPath: '/dist/',
-    port: 3000
-  }
+    port: 3000,
+  },
 };
