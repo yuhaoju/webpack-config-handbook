667015796243e56f0d56aa4238767b0ca1b872e8
Yuhao Ju <yuhaoju0623@gmail.com>
2019-02-16 19:49:10 +0800

    chore: update deps

--- 01-get-started/hello-webpack/webpack.config.js
+++ 01-get-started/hello-webpack/webpack.config.js
@@ -4,11 +4,10 @@ const htmlPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, 'asset'),
     filename: 'bundle.js',
   },
   mode: 'development',
-  // plugins: [new htmlPlugin({ title: path.basename(__dirname), })],
+  plugins: [new htmlPlugin({ title: path.basename(__dirname), })],
   devServer: {
     port: 3000,
   },
